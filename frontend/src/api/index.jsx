255b345e16405caf68d618fd70c545a3c358540c
Harshit-Banwal <harshitbanwal849@gmail.com>
2024-03-28 22:39:01 +0530

    backend url changes

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://task-manager-api-lac.vercel.app/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-manager-api-lac.vercel.app/api',
+  // baseURL: 'http://localhost:5000/api',
 });
 export default api;
